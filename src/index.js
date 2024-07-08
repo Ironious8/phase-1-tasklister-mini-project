8e1b22ea5f0e88828ee6d83b94385cb2f5fcf034
Ironious8 <tyronekariuki@gmail.com>
2024-07-08 13:10:13 +0300

    Add tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+function createTask(event) {
+  event.preventDefault();
+  let ul=document.getElementById('tasks');
+  let li=document.createElement('li');
+  let description=document.getElementById('new-task-description');
+  li.setAttribute('id', description.value);
+  li.appendChild(document.createTextNode(description.value));
+  ul.appendChild('li');
+}
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    createTask();
+});
+
+function deleteTask() {
+  let ul=document.getElementById('tasks');
+  ul.remove();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  deleteTask();
 });
